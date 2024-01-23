3ce5a526c11d8ddc6f493b269c6595f0be4e21db
sishir44 <sishirbohara4@gmail.com>
2024-01-23 22:18:58 +0530

    get record using primary key

--- DbOperationsWithEFCoreApp/Controllers/LanguageController.cs
+++ DbOperationsWithEFCoreApp/Controllers/LanguageController.cs
@@ -16,10 +16,19 @@ namespace DbOperationsWithEFCoreApp.Controllers
             _appDbContext = appDbContext;
         }
 
+        // to get List of records
         [HttpGet("")]
         public async Task<IActionResult> GetAllLanguages()
         {
             var result = await (from Languages in _appDbContext.Languages select Languages).ToListAsync();
+            return Ok(result);
+        }
+
+        // get record using Primary Key
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLanguageByIdAsyns([FromRoute] int id)
+        {
+            var result = await _appDbContext.Languages.FindAsync(id);
             return Ok(result);
         }
 
